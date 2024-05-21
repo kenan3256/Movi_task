8e99e2e70ec3e73803ce6c2f76942ebc321522b6
kenan.haciyev <kenanhaciyev85@gmail.com>
2024-05-21 17:29:05 +0400

    END_add_Login_page

---
+++ app/src/main/java/com/kenanhaciyev/movi_task/fragment/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.kenanhaciyev.movi_task.ui.adapter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kenanhaciyev.movi_task.ARG_PARAM1
+import com.kenanhaciyev.movi_task.ARG_PARAM2
+import com.kenanhaciyev.movi_task.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login2, container, false)
+    }
+
+}
